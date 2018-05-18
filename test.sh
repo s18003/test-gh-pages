82c0b406200b2c55990c42a34627596ef1636dd9
s18003 <s18003@std.it-college.ac.jp>
2018-05-18 05:21:36 +0000

    index.htmlを新規作成

--- test.sh
+++ test.sh
@@ -8,6 +8,8 @@ if [ $today -eq 20 ]; then
   echo "今日は20日です。月次〆処理を開始してください"
 elif [ $today -lt 20 ]; then
   echo "今日は20日より前の日にちです。"
+elif [ $today -gt 20 ]; then
+  echo "今日は20日より後の日にちです。"
 else
-  echo "今日は20日ではありません。"
+  echo "err:日付が取れませんでした。"
 fi
